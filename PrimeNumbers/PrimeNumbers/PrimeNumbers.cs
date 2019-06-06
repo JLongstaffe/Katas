f8ddd35ab2f585a1eda0c3c116a33b9ebe999de3
James Longstaffe <james.longstaffe@red-gate.com>
2019-06-06 15:54:17 +0100

    Add Prime Numbers

---
+++ PrimeNumbers/PrimeNumbers/PrimeNumbers.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrimeNumbers
+{
+    /// <summary>
+    /// Finds the nth prime number using the Sieve of Eratosthenes
+    /// </summary>
+    ///
+    public class PrimeNumbers
+    {
+        /// <param name="maxPrime">
+        /// The maximum possible prime number than can be found
+        /// </param>
+        ///
+        public PrimeNumbers(int maxPrime)
+        {
+            if (maxPrime <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(maxPrime));
+            }
+
+            m_Sieve = new bool[maxPrime];
+        }
+
+        /// <returns>
+        /// The nth prime number, or 0 if the prime could not be found within
+        /// the maximum configured range.
+        /// </returns>
+        ///
+        public int GetPrime(int n) => Primes().Skip(n - 1)
+                                              .FirstOrDefault();
+
+        private IEnumerable<int> Primes()
+        {
+            for (var candidate = 2; candidate < m_Sieve.Length; candidate++)
+            {
+                if (m_Sieve[candidate]) continue;
+
+                yield return candidate;
+
+                for (var multiple = 1; candidate * multiple < m_Sieve.Length; multiple++)
+                {
+                    m_Sieve[candidate * multiple] = true;
+                }
+            }
+        }
+
+        private readonly bool[] m_Sieve;
+    }
+}
---
+++ PrimeNumbers/PrimeNumbers/PrimeNumbersTests.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+using System.Diagnostics;
+
+using NUnit.Framework;
+
+namespace PrimeNumbers
+{
+    internal sealed class PrimeNumbersTests
+    {
+        [TestCase(0)]
+        [TestCase(-1)]
+        public void Cannot_contruct_with_invalid_arguments(int maxPrime)
+        {
+            Assert.That(() => new PrimeNumbers(maxPrime),
+                        Throws.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        public void Returns_zero_if_prime_not_found_within_configured_range()
+        {
+            Assert.That(() => new PrimeNumbers(2).GetPrime(2), Is.EqualTo(0));
+        }
+
+        [TestCase(  1,   ExpectedResult =   2)]
+        [TestCase(  2,   ExpectedResult =   3)]
+        [TestCase(  3,   ExpectedResult =   5)]
+        [TestCase(  4,   ExpectedResult =   7)]
+        [TestCase(  5,   ExpectedResult =  11)]
+        [TestCase( 10,   ExpectedResult =  29)]
+        [TestCase( 20,   ExpectedResult =  71)]
+        [TestCase(100,   ExpectedResult = 541)]
+        public int Returns_nth_prime(int n)
+            => new PrimeNumbers(1000).GetPrime(n);
+
+        [Test]
+        public void Benchmark()
+        {
+            var primeNumbers = new PrimeNumbers(5_000_000);
+
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            Assert.That(primeNumbers.GetPrime(100_000), Is.EqualTo(1_299_709));
+
+            stopwatch.Stop();
+
+            Assert.That(stopwatch.ElapsedMilliseconds, Is.LessThan(100));
+        }
+    }
+}
