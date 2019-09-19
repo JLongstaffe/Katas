53e819543f1e8ce85c425f4f1b4b64857a1d5b8d
James Longstaffe <james.longstaffe@red-gate.com>
2019-09-19 18:15:55 +0100

    Initial commit

---
+++ Fibonacci/Fibonacci/Fibonacci.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fibonacci
+{
+    internal sealed class Fibonacci
+    {
+        public static long Get(int n)
+        {
+            if (n < 0) throw new ArgumentNullException(nameof(n));
+
+            return Series().Skip(n).First();
+        }
+
+        private static IEnumerable<long> Series()
+        {
+            var (current, next) = ((long) 0, (long) 1);
+
+            while (true)
+            {
+                yield return current;
+
+                (current, next) = (next, current + next);
+            }
+        }
+    }
+}
---
+++ Fibonacci/Fibonacci/FibonacciTests.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using System.Diagnostics;
+
+using NUnit.Framework;
+
+namespace Fibonacci
+{
+    internal sealed class FibonacciTests
+    {
+        [TestCase(-2)]
+        [TestCase(-1)]
+        public void Rejects_negative_numbers(int n)
+        {
+            Assert.That(() => Fibonacci.Get(n),
+                        Throws.InstanceOf<ArgumentNullException>());
+        }
+
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        [TestCase(2, 1)]
+        [TestCase(3, 2)]
+        [TestCase(5, 5)]
+        [TestCase(20, 6765)]
+        public void Returns_nth_fibonacci_number(int n, long expected)
+        {
+            Assert.That(Fibonacci.Get(n), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Benchmark()
+        {
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            var result = Fibonacci.Get(90);
+
+            stopwatch.Stop();
+
+            Assert.That(result, Is.EqualTo(2_880_067_194_370_816_120));
+
+            Assert.That(stopwatch.ElapsedMilliseconds, Is.LessThan(5));
+        }
+    }
+}
