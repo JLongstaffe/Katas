5e3f4a4310db2c1dbd9a3dbedc9f314687f864d0
James Longstaffe <james.longstaffe@red-gate.com>
2019-09-27 16:39:04 +0100

    Add C# implementation of string permutations

---
+++ Permutations/Permutations/Permutations.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Permutations
+{
+    public static class Permutations
+    {
+        public static IReadOnlyCollection<string> Of(string input)
+        {
+            return Of(input, string.Empty, new List<string>());
+        }
+
+        private static IReadOnlyCollection<string> Of
+            (string input,
+             string prefix,
+             IList<string> results)
+        {
+            if (input.Length == 0) results.Add(prefix);
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                Of(input.Substring(0, i) + input.Substring(i + 1),
+                   prefix + input[i],
+                   results);
+            }
+
+            return results.ToArray();
+        }
+    }
+}
---
+++ Permutations/Permutations/PermutationsTests.cs
@@ -0,0 +1,44 @@
+﻿
+using System.Diagnostics;
+using System.Linq;
+
+using NUnit.Framework;
+
+namespace Permutations
+{
+    internal sealed class PermutationsTests
+    {
+        [TestCase("",     new [] { "" })]
+        [TestCase("ab",   new [] { "ab", "ba" })]
+        [TestCase("abc",  new [] { "abc", "acb", "bac", "bca", "cab", "cba" })]
+        [TestCase("abcd", new []
+        {
+            "abcd", "abdc", "acbd", "acdb", "adbc", "adcb",
+            "bacd", "badc", "bcad", "bcda", "bdac", "bdca",
+            "cabd", "cadb", "cbad", "cbda", "cdab", "cdba",
+            "dabc", "dacb", "dbac", "dbca", "dcab", "dcba"
+        })]
+        public void Returns_correct_permutations(string input, string[] expected)
+        {
+            Assert.That(Permutations.Of(input), Is.EquivalentTo(expected));
+        }
+
+        [Test]
+        public void Benchmark()
+        {
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            var result = Permutations.Of("01234567");
+
+            stopwatch.Stop();
+
+            Assert.That(result.Contains("01234567"));
+
+            Assert.That(result.Count, Is.EqualTo(40_320)); // 8!
+
+            Assert.That(stopwatch.ElapsedMilliseconds, Is.LessThan(10_000));
+        }
+    }
+}
