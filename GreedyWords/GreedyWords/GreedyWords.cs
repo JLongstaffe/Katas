bfb0856b20e7f2f48f115cc5d1cf491e8594e08b
James Longstaffe <james.longstaffe@red-gate.com>
2019-10-15 18:42:04 +0100

    Initial implementation of GreedyWords

---
+++ GreedyWords/GreedyWords/GreedyWords.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GreedyWords
+{
+    public class GreedyWords
+    {
+        public GreedyWords(ISet<string> dictionary)
+        {
+            m_Dictionary = dictionary
+                        ?? throw new ArgumentNullException(nameof(dictionary));
+        }
+
+        public IReadOnlyCollection<string> GetWords(string input)
+        {
+            if (input is null)
+            {
+                throw new ArgumentNullException(nameof(input));
+            }
+
+            return GetWords(input, new Stack<string>()).FirstOrDefault();
+        }
+
+        private IEnumerable<IReadOnlyCollection<string>> GetWords
+            (string input,
+             Stack<string> words)
+        {
+            if (m_Dictionary.Contains(input))
+            {
+                words.Push(input);
+
+                yield return words.AsEnumerable().Reverse().ToArray();
+
+                words.Pop();
+            }
+
+            for (var i = input.Length - 1; i > 0; i--)
+            {
+                if (m_Dictionary.Contains(input.Substring(0, i)))
+                {
+                    words.Push(input.Substring(0, i));
+
+                    foreach (var result in GetWords(input.Substring(i), words))
+                    {
+                        yield return result;
+                    }
+                }
+            }
+
+            if (words.Count > 0) words.Pop();
+        }
+
+        private readonly ISet<string> m_Dictionary;
+    }
+}
