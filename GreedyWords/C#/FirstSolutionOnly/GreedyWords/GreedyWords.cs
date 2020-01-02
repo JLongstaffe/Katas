268275863c1f95b344edb4aaad6f713d4808c151
James Longstaffe <james.longstaffe@red-gate.com>
2020-01-02 14:02:22 +0000

    Add alternate implementation of greedy words for first solution only

---
+++ GreedyWords/C#/FirstSolutionOnly/GreedyWords/GreedyWords.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GreedyWords
+{
+    public class GreedyWords
+    {
+        public GreedyWords(ISet<string> dictionary)
+        {
+            m_Dictionary = dictionary
+                        ?? throw new ArgumentNullException(nameof(dictionary));
+        }
+
+        public IReadOnlyCollection<string> GetWords(string input)
+        {
+            if (input is null)
+            {
+                throw new ArgumentNullException(nameof(input));
+            }
+
+            var words = new Stack<string>();
+
+            return GetWords(input, words)
+                 ? words.AsEnumerable().Reverse().ToArray()
+                 : null;
+        }
+
+        private bool GetWords(string input, Stack<string> words)
+        {
+            if (m_Dictionary.Contains(input))
+            {
+                words.Push(input);
+
+                return true;
+            }
+
+            for (var i = input.Length - 1; i > 0; i--)
+            {
+                if (!m_Dictionary.Contains(input.Substring(0, i))) continue;
+
+                words.Push(input.Substring(0, i));
+
+                if (GetWords(input.Substring(i), words)) return true;
+            }
+
+            if (words.Count > 0) words.Pop();
+
+            return false;
+        }
+
+        private readonly ISet<string> m_Dictionary;
+    }
+}
---
+++ GreedyWords/C#/FirstSolutionOnly/GreedyWords/GreedyWordsTests.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+namespace GreedyWords
+{
+    public sealed class GreedyWordsTests
+    {
+        [Test]
+        public void Cannot_construct_with_null_dictionary()
+        {
+            Assert.That(() => new GreedyWords(null),
+                        Throws.TypeOf<ArgumentNullException>());
+        }
+
+        [TestCase("")]
+        [TestCase("a")]
+        [TestCase("abc")]
+        public void Returns_null_if_no_solution(string input)
+        {
+            var dictionary = new HashSet<string> { "ab", "d", "bc" };
+
+            var greedyWords = new GreedyWords(dictionary);
+
+            Assert.That(greedyWords.GetWords(input), Is.Null);
+        }
+
+        [TestCase("helloworld", "hello", "world")]
+        [TestCase("thequickbrownfoxjumpsoverthelazydog",
+                  "the", "quick", "brown", "fox", "jumps",
+                  "overt", "he", "lazy", "dog")]
+        [TestCase("brownerd", "brow", "nerd")]
+        public void Returns_correct_words_matched_greedily
+            (string input, params string[] expected)
+        {
+            var dictionary = new HashSet<string>
+                { "he", "hell", "hello", "low", "or", "world",
+                  "the", "quick", "bro", "brow", "brown", "own", "owner",
+                  "fox", "ox", "jump", "jumps", "over", "overt", "la",
+                  "lazy", "do", "dog", "nerd" };
+
+            var greedyWords = new GreedyWords(dictionary);
+
+            Assert.That(greedyWords.GetWords(input), Is.EqualTo(expected));
+        }
+    }
+}
