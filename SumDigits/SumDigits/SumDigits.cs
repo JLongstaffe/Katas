5579d44d37103ca9086772faeea9d9727c6e1082
jameslongstaffe <james.longstaffe@red-gate.com>
2020-03-23 14:52:43 +0000

    Sum Digits (naiive)

---
+++ SumDigits/SumDigits/SumDigits.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SumDigits
+{
+    internal static class SumDigits
+    {
+        public static int Sum(int n)
+        {
+            return Enumerable.Range(1, n)
+                  .SelectMany(ToDigits)
+                  .Sum();
+        }
+
+        private static IEnumerable<int> ToDigits(int n)
+        {
+            for (var i = n; i > 0; i /= 10)
+            {
+                yield return i % 10;
+            }
+        }
+    }
+}
---
+++ SumDigits/SumDigits/SumDigitsTests.cs
@@ -0,0 +1,17 @@
+﻿
+using NUnit.Framework;
+
+namespace SumDigits
+{
+    internal class SumDigitsTests
+    {
+        [TestCase(0,  ExpectedResult = 0)]
+        [TestCase(9,  ExpectedResult = 45)]
+        [TestCase(10, ExpectedResult = 46)]
+        [TestCase(15, ExpectedResult = 66)]
+        public int Sums_all_digits(int n)
+        {
+            return SumDigits.Sum(n);
+        }
+    }
+}
